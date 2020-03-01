e89ad754ad64fb45eef5ea050289866a7676f151
Elijah Mooring <45398751+Vehmloewff@users.noreply.github.com>
2020-02-29 21:25:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const port = process.env.PORT || 4000
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello from test-app!\n');
+    res.end('Hello from test-app!\nDoes this work?');
 }).listen(port, "127.0.0.1");
 console.log(`Server running at http://127.0.0.1:${port}/`);
